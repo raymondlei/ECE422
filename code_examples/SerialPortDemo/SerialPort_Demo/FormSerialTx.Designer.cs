be4a26d94a0d88f6f6fe1a16a24377c697ffb3f5
Raymond Lee <riemann.lei@gmail.com>
2018-08-03 16:04:42 -0500

    serial port transmit and receive demo;

---
+++ code_examples/SerialPortDemo/SerialPort_Demo/FormSerialTx.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SerialPort_Demo
+{
+    partial class FormSerialTx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbAscii = new System.Windows.Forms.TextBox();
+            this.lblAsciiString = new System.Windows.Forms.Label();
+            this.tbHex = new System.Windows.Forms.TextBox();
+            this.lblHex = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.comPortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbAutoSend = new System.Windows.Forms.CheckBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbAscii
+            // 
+            this.tbAscii.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAscii.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbAscii.Font = new System.Drawing.Font("Segoe WP", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAscii.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.tbAscii.Location = new System.Drawing.Point(12, 62);
+            this.tbAscii.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbAscii.Multiline = true;
+            this.tbAscii.Name = "tbAscii";
+            this.tbAscii.Size = new System.Drawing.Size(487, 181);
+            this.tbAscii.TabIndex = 0;
+            this.tbAscii.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbAscii_KeyPress);
+            // 
+            // lblAsciiString
+            // 
+            this.lblAsciiString.AutoSize = true;
+            this.lblAsciiString.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsciiString.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblAsciiString.Location = new System.Drawing.Point(12, 33);
+            this.lblAsciiString.Name = "lblAsciiString";
+            this.lblAsciiString.Size = new System.Drawing.Size(115, 25);
+            this.lblAsciiString.TabIndex = 1;
+            this.lblAsciiString.Text = "ASCII String";
+            // 
+            // tbHex
+            // 
+            this.tbHex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbHex.BackColor = System.Drawing.SystemColors.Info;
+            this.tbHex.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbHex.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.tbHex.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHex.Location = new System.Drawing.Point(17, 275);
+            this.tbHex.Multiline = true;
+            this.tbHex.Name = "tbHex";
+            this.tbHex.ReadOnly = true;
+            this.tbHex.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbHex.Size = new System.Drawing.Size(487, 83);
+            this.tbHex.TabIndex = 2;
+            this.tbHex.TabStop = false;
+            // 
+            // lblHex
+            // 
+            this.lblHex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblHex.AutoSize = true;
+            this.lblHex.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHex.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblHex.Location = new System.Drawing.Point(12, 247);
+            this.lblHex.Name = "lblHex";
+            this.lblHex.Size = new System.Drawing.Size(46, 25);
+            this.lblHex.TabIndex = 3;
+            this.lblHex.Text = "Hex";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSend.FlatAppearance.BorderColor = System.Drawing.Color.LightSkyBlue;
+            this.btnSend.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gainsboro;
+            this.btnSend.FlatAppearance.MouseOverBackColor = System.Drawing.Color.AliceBlue;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Segoe WP", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnSend.Location = new System.Drawing.Point(505, 62);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(112, 62);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comPortToolStripMenuItem,
+            this.clearTextToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // comPortToolStripMenuItem
+            // 
+            this.comPortToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.comPortToolStripMenuItem.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.comPortToolStripMenuItem.Name = "comPortToolStripMenuItem";
+            this.comPortToolStripMenuItem.Size = new System.Drawing.Size(75, 21);
+            this.comPortToolStripMenuItem.Text = "&Com Port";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.connectToolStripMenuItem.Text = "&Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.disconnectToolStripMenuItem.Text = "&Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // clearTextToolStripMenuItem
+            // 
+            this.clearTextToolStripMenuItem.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.clearTextToolStripMenuItem.Name = "clearTextToolStripMenuItem";
+            this.clearTextToolStripMenuItem.Size = new System.Drawing.Size(78, 21);
+            this.clearTextToolStripMenuItem.Text = "Clear Te&xt";
+            this.clearTextToolStripMenuItem.Click += new System.EventHandler(this.clearTextToolStripMenuItem_Click);
+            // 
+            // cbAutoSend
+            // 
+            this.cbAutoSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbAutoSend.AutoSize = true;
+            this.cbAutoSend.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbAutoSend.Location = new System.Drawing.Point(520, 178);
+            this.cbAutoSend.Name = "cbAutoSend";
+            this.cbAutoSend.Size = new System.Drawing.Size(97, 24);
+            this.cbAutoSend.TabIndex = 6;
+            this.cbAutoSend.Text = "Auto-send";
+            this.cbAutoSend.UseVisualStyleBackColor = true;
+            this.cbAutoSend.CheckedChanged += new System.EventHandler(this.cbAutoSend_CheckedChanged);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // FormSerialTx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(629, 370);
+            this.Controls.Add(this.cbAutoSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblHex);
+            this.Controls.Add(this.tbHex);
+            this.Controls.Add(this.lblAsciiString);
+            this.Controls.Add(this.tbAscii);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormSerialTx";
+            this.Text = "Serial Port Tx Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormSerialTx_FormClosing);
+            this.Load += new System.EventHandler(this.FormSerialTx_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAscii;
+        private System.Windows.Forms.Label lblAsciiString;
+        private System.Windows.Forms.TextBox tbHex;
+        private System.Windows.Forms.Label lblHex;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem comPortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.CheckBox cbAutoSend;
+        private System.Windows.Forms.ToolStripMenuItem clearTextToolStripMenuItem;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ code_examples/SerialPortDemo/SerialPort_Demo/FormSerialTx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPort_Demo
+{
+    /// <summary>
+    /// Simple Serial Communication
+    /// 
+    /// Reference URLs:
+    /// 
+    /// Example codes: http://csharp.simpleserial.com/
+    /// Official MSDN documentation: https://msdn.microsoft.com/en-us/library/system.io.ports.serialport.aspx
+    /// 
+    /// Keywords: "C# SerialPort", "C# serial port"
+    /// </summary>
+    public partial class FormSerialTx : Form
+    {
+        private bool _autosendEnabled;
+        private string _rxString;
+
+        public FormSerialTx()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect(SerialPort handle)
+        {
+            //Configure serial port
+            serialPort1.PortName = "COM1";
+            serialPort1.BaudRate = 9600;
+
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.Open();  //attempt to open the configured serial port
+                    if (serialPort1.IsOpen)
+                    {
+                        //Com Port is connected
+                        connectToolStripMenuItem.Enabled = false;
+                        disconnectToolStripMenuItem.Enabled = true;
+                        tbAscii.ReadOnly = false;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unable to open serial port");
+                }
+            }
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //handle serial port connect
+            Connect(serialPort1);
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                //Disconnect serial port
+                serialPort1.Close();
+                connectToolStripMenuItem.Enabled = true;
+                disconnectToolStripMenuItem.Enabled = false;
+                tbAscii.ReadOnly = true;
+            }
+        }
+
+        private void FormSerialTx_Load(object sender, EventArgs e)
+        {
+            _autosendEnabled = cbAutoSend.Checked;
+        }
+
+        private void FormSerialTx_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Note: SerialPort object operates on a seperate thread.
+            //      Therefore, DataReceived event can not interact directly with other WinForm controls.
+            //      Doing so will cause cross-thread action exception.
+            //      In order to display received data to UI, delegate method must be used.
+
+            //Read data from serial port object
+            string data = serialPort1.ReadExisting();
+
+            //Choose one of the approaches below
+
+            //Approach 1: call Invoke method and pass data via external variable
+            _rxString = data;  //assign to external variable
+            this.Invoke(new EventHandler(ShowText));
+
+            //Approach 2: pass data via parameter
+            //this.Invoke(new EventHandler(DisplayText), new object[] { data });
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            tbAscii.AppendText((string)sender);
+        }
+
+        private void ShowText(object sender, EventArgs e)
+        {
+            tbAscii.AppendText(_rxString);
+        }
+
+        private void tbAscii_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char[] key = new char[1];
+            key[0] = e.KeyChar;  //Read keypress character
+
+            byte[] buffer = Encoding.ASCII.GetBytes(key);
+            if (buffer.Length > 0)
+                UpdateTextbox(buffer);
+
+            if (_autosendEnabled)
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write(key, 0, 1);
+                }
+            }
+
+            // Set the KeyPress event as handled so the character won't
+            // display locally. If you want it to display, comment out the next line.
+            //e.Handled = true;
+        }
+
+        private void UpdateTextbox(byte[] buffer)
+        {
+            tbHex.Text += ByteArrayToString(buffer);
+            tbHex.SelectionStart = tbHex.Text.Length;
+            tbHex.ScrollToCaret();
+            tbHex.Refresh();
+        }
+
+        private void cbAutoSend_CheckedChanged(object sender, EventArgs e)
+        {
+            _autosendEnabled = cbAutoSend.Checked;
+        }
+
+        private void clearTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tbAscii.Clear();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (string.IsNullOrEmpty(tbAscii.Text) == false)
+                {
+                    //Write string directly to serial port object
+                    serialPort1.Write(tbAscii.Text);
+                }
+            }
+        }
+
+        private static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:X2} ", b);
+            return hex.ToString();
+        }
+    }
+}
