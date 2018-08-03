cbf39fabba7f5830988f475295d6f10184c2cab9
Raymond Lee <riemann.lei@gmail.com>
2018-08-03 16:23:25 -0500

    Added exercise - Vending Machine;

---
+++ code_examples/VendingMachine_blank/vending/FormMain.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace vending
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1Cent = new System.Windows.Forms.Button();
+            this.btn5Cent = new System.Windows.Forms.Button();
+            this.btn10Cent = new System.Windows.Forms.Button();
+            this.btn25Cent = new System.Windows.Forms.Button();
+            this.btn1Dollar = new System.Windows.Forms.Button();
+            this.btnReturnCoin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSnack = new System.Windows.Forms.Button();
+            this.btnCoffee = new System.Windows.Forms.Button();
+            this.btnIceCream = new System.Windows.Forms.Button();
+            this.btnSandwich = new System.Windows.Forms.Button();
+            this.btnTaco = new System.Windows.Forms.Button();
+            this.btnSoda = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn1Cent
+            // 
+            this.btn1Cent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn1Cent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn1Cent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1Cent.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1Cent.Location = new System.Drawing.Point(64, 303);
+            this.btn1Cent.Name = "btn1Cent";
+            this.btn1Cent.Size = new System.Drawing.Size(52, 32);
+            this.btn1Cent.TabIndex = 0;
+            this.btn1Cent.Text = "1c";
+            this.btn1Cent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1Cent.UseVisualStyleBackColor = false;
+            // 
+            // btn5Cent
+            // 
+            this.btn5Cent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn5Cent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn5Cent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5Cent.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5Cent.Location = new System.Drawing.Point(131, 303);
+            this.btn5Cent.Name = "btn5Cent";
+            this.btn5Cent.Size = new System.Drawing.Size(52, 32);
+            this.btn5Cent.TabIndex = 1;
+            this.btn5Cent.Text = "5c";
+            this.btn5Cent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn5Cent.UseVisualStyleBackColor = false;
+            // 
+            // btn10Cent
+            // 
+            this.btn10Cent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn10Cent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn10Cent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn10Cent.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10Cent.Location = new System.Drawing.Point(199, 303);
+            this.btn10Cent.Name = "btn10Cent";
+            this.btn10Cent.Size = new System.Drawing.Size(52, 32);
+            this.btn10Cent.TabIndex = 2;
+            this.btn10Cent.Text = "10c";
+            this.btn10Cent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn10Cent.UseVisualStyleBackColor = false;
+            // 
+            // btn25Cent
+            // 
+            this.btn25Cent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn25Cent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn25Cent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn25Cent.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn25Cent.Location = new System.Drawing.Point(131, 354);
+            this.btn25Cent.Name = "btn25Cent";
+            this.btn25Cent.Size = new System.Drawing.Size(52, 32);
+            this.btn25Cent.TabIndex = 3;
+            this.btn25Cent.Text = "25c";
+            this.btn25Cent.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn25Cent.UseVisualStyleBackColor = false;
+            // 
+            // btn1Dollar
+            // 
+            this.btn1Dollar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn1Dollar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn1Dollar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1Dollar.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1Dollar.Location = new System.Drawing.Point(199, 354);
+            this.btn1Dollar.Name = "btn1Dollar";
+            this.btn1Dollar.Size = new System.Drawing.Size(52, 32);
+            this.btn1Dollar.TabIndex = 4;
+            this.btn1Dollar.Text = "$1";
+            this.btn1Dollar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn1Dollar.UseVisualStyleBackColor = false;
+            // 
+            // btnReturnCoin
+            // 
+            this.btnReturnCoin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReturnCoin.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnReturnCoin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReturnCoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturnCoin.Location = new System.Drawing.Point(277, 274);
+            this.btnReturnCoin.Name = "btnReturnCoin";
+            this.btnReturnCoin.Size = new System.Drawing.Size(94, 112);
+            this.btnReturnCoin.TabIndex = 5;
+            this.btnReturnCoin.Text = "Return Coins";
+            this.btnReturnCoin.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 28);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("OCR A Extended", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(277, 243);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 25);
+            this.textBox2.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.Color.DimGray;
+            this.groupBox1.Controls.Add(this.btnSnack);
+            this.groupBox1.Controls.Add(this.btnCoffee);
+            this.groupBox1.Controls.Add(this.btnIceCream);
+            this.groupBox1.Controls.Add(this.btnSandwich);
+            this.groupBox1.Controls.Add(this.btnTaco);
+            this.groupBox1.Controls.Add(this.btnSoda);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 222);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSnack
+            // 
+            this.btnSnack.BackColor = System.Drawing.Color.OldLace;
+            this.btnSnack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSnack.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSnack.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSnack.Location = new System.Drawing.Point(258, 125);
+            this.btnSnack.Name = "btnSnack";
+            this.btnSnack.Size = new System.Drawing.Size(90, 60);
+            this.btnSnack.TabIndex = 5;
+            this.btnSnack.Text = "Snack";
+            this.btnSnack.UseVisualStyleBackColor = false;
+            // 
+            // btnCoffee
+            // 
+            this.btnCoffee.BackColor = System.Drawing.Color.OldLace;
+            this.btnCoffee.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCoffee.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCoffee.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnCoffee.Location = new System.Drawing.Point(258, 34);
+            this.btnCoffee.Name = "btnCoffee";
+            this.btnCoffee.Size = new System.Drawing.Size(90, 60);
+            this.btnCoffee.TabIndex = 4;
+            this.btnCoffee.Text = "Coffee";
+            this.btnCoffee.UseVisualStyleBackColor = false;
+            // 
+            // btnIceCream
+            // 
+            this.btnIceCream.BackColor = System.Drawing.Color.OldLace;
+            this.btnIceCream.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIceCream.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIceCream.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnIceCream.Location = new System.Drawing.Point(132, 125);
+            this.btnIceCream.Name = "btnIceCream";
+            this.btnIceCream.Size = new System.Drawing.Size(90, 60);
+            this.btnIceCream.TabIndex = 3;
+            this.btnIceCream.Text = "Ice Cream";
+            this.btnIceCream.UseVisualStyleBackColor = false;
+            // 
+            // btnSandwich
+            // 
+            this.btnSandwich.BackColor = System.Drawing.Color.OldLace;
+            this.btnSandwich.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSandwich.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSandwich.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSandwich.Location = new System.Drawing.Point(132, 34);
+            this.btnSandwich.Name = "btnSandwich";
+            this.btnSandwich.Size = new System.Drawing.Size(90, 60);
+            this.btnSandwich.TabIndex = 2;
+            this.btnSandwich.Text = "Sandwich";
+            this.btnSandwich.UseVisualStyleBackColor = false;
+            // 
+            // btnTaco
+            // 
+            this.btnTaco.BackColor = System.Drawing.Color.OldLace;
+            this.btnTaco.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTaco.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaco.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnTaco.Location = new System.Drawing.Point(8, 125);
+            this.btnTaco.Name = "btnTaco";
+            this.btnTaco.Size = new System.Drawing.Size(90, 60);
+            this.btnTaco.TabIndex = 1;
+            this.btnTaco.Text = "Taco";
+            this.btnTaco.UseVisualStyleBackColor = false;
+            // 
+            // btnSoda
+            // 
+            this.btnSoda.BackColor = System.Drawing.Color.OldLace;
+            this.btnSoda.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSoda.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoda.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSoda.Location = new System.Drawing.Point(8, 34);
+            this.btnSoda.Name = "btnSoda";
+            this.btnSoda.Size = new System.Drawing.Size(90, 60);
+            this.btnSoda.TabIndex = 0;
+            this.btnSoda.Text = "Soda";
+            this.btnSoda.UseVisualStyleBackColor = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(383, 398);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnReturnCoin);
+            this.Controls.Add(this.btn1Dollar);
+            this.Controls.Add(this.btn25Cent);
+            this.Controls.Add(this.btn10Cent);
+            this.Controls.Add(this.btn5Cent);
+            this.Controls.Add(this.btn1Cent);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FormMain";
+            this.Text = "Vending Machine";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1Cent;
+        private System.Windows.Forms.Button btn5Cent;
+        private System.Windows.Forms.Button btn10Cent;
+        private System.Windows.Forms.Button btn25Cent;
+        private System.Windows.Forms.Button btn1Dollar;
+        private System.Windows.Forms.Button btnReturnCoin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSnack;
+        private System.Windows.Forms.Button btnCoffee;
+        private System.Windows.Forms.Button btnIceCream;
+        private System.Windows.Forms.Button btnSandwich;
+        private System.Windows.Forms.Button btnTaco;
+        private System.Windows.Forms.Button btnSoda;
+    }
+}
+
